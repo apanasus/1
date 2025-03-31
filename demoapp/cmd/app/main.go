f6a66aad01142bdd3af997091b73f4f3fcc504be
apanasus <43000960+apanasus@users.noreply.github.com>
2025-03-31 23:30:29 +0300

    reverse string web in go

--- demoapp/cmd/app/main.go
+++ demoapp/cmd/app/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"demoapp/pkg/stringutils"
+	"net/http"
+)
 
+func main() {
+	http.ListenAndServe(
+		":8080",
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				s := r.URL.Query()["s"]
+				if len(s) == 0 {
+					http.Error(w, "bad query", http.StatusBadRequest)
+				}
+
+				rev := stringutils.Rev(s[0])
+
+				w.Write([]byte(rev))
+			},
+		),
+	)
 }
--- demoapp/pkg/stringutils/stringutils_test.go
+++ demoapp/pkg/stringutils/stringutils_test.go
@@ -3,19 +3,25 @@ package stringutils
 import "testing"
 
 func TestRev(t *testing.T) {
-	type args struct {
-		s string
-	}
 	tests := []struct {
 		name string
-		args args
+		s    string
 		want string
 	}{
-		// TODO: Add test cases.
+		{
+			name: "test 1",
+			s:    "ABC",
+			want: "CBA",
+		},
+		{
+			name: "test 1",
+			s:    "АБВ",
+			want: "ВБА",
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := Rev(tt.args.s); got != tt.want {
+			if got := Rev(tt.s); got != tt.want {
 				t.Errorf("Rev() = %v, want %v", got, tt.want)
 			}
 		})
