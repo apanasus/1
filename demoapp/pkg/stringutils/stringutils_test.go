868c3e3e4f1df8331e4c8075a865f5ea79c089c6
apanasus <43000960+apanasus@users.noreply.github.com>
2025-03-31 22:57:20 +0300

    init

---
+++ demoapp/pkg/stringutils/stringutils_test.go
@@ -0,0 +1,23 @@
+package stringutils
+
+import "testing"
+
+func TestRev(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Rev(tt.args.s); got != tt.want {
+				t.Errorf("Rev() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
